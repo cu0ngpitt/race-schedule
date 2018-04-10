218b7e0b9c72ff6533d596fe2134dccce282ca07
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-04-10 13:20:39 -0500

    moved data to new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,37 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ultra Racing Schedule';
-  races = [{
-    "id": 1,
-    "name": "Daytona Thunderdome",
-    "date": new Date('2512-01-04T14:00:00'),
-    "about": "Race through the ruins of an ancient Florida battle arena.",
-    "entryFee": 3200,
-    "isRacing": false
-    }, {
-    "id": 2,
-    "name": "San Francisco Ruins",
-    "date": new Date('2512-07-03T20:00:00'),
-    "about": "Drift down the streets of a city almost sunk under the ocean.",
-    "entryFee": 4700,
-    "isRacing": true
-    }, {
-    "id": 3,
-    "name": "New York City Skyline",
-    "date": new Date('2512-07-12T21:00:00'),
-    "about": "Fly between buildings in the electronic sky.",
-    "entryFee": 4300,
-    "isRacing": false
-    }
-  ]
-
-    totalCost() {
-      let sum = 0;
-        for (let race of this.races) {
-          if (race.isRacing) sum += race.entryFee;
-        }
-      return sum;
-}
-}
-
 }
